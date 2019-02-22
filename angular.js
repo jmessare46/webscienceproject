1a9da13f187129253dcb30ee3f6cac31c801da48
jmessare46 <jmessare46@gmail.com>
2019-02-22 13:30:37 -0500

    Seperated navigation on the site

---
+++ angular.js
@@ -0,0 +1,10 @@
+// Instantiate the myApp Angular application that we attached to out html tag
+var app = angular.module("myApp", []);
+
+// Here is the Javascript for our controller which we linked (scoped) to the body tag
+app.controller("mainController", ['$scope','$http',function($scope, $http) {
+
+    // Lets setup some variables for our expressions
+    $scope.navbar = "navigation.html";
+
+}]);
