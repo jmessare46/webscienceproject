6b8f142d04ed4f520d32f208551a1981e314f2e6
arronlook <33300146+arronlook@users.noreply.github.com>
2019-02-26 21:41:11 -0500

    Finished rough Inventory Page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/')); // Might want to limit the folders instead of having it at root
 app.get('/inventory', (req, res)=>
 {
   res.sendFile(__dirname + '/inventory.html');
