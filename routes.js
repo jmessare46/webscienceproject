b8db111f083f8316aad167cee82aaa22aa1d793f
jmessare46 <jmessare46@gmail.com>
2019-04-19 12:37:33 -0400

    Optimized use of DB connections

--- routes.js
+++ routes.js
@@ -4,6 +4,28 @@ const Mclient = require("mongodb").MongoClient;
 const saltRounds = 10;
 const bcrypt = require('bcrypt');
 const ObjectId = require('mongodb').ObjectId;
+const c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
+var users;
+var shops;
+var products;
+
+c4.connect((err)=>
+{
+    if (err)
+    {
+        console.log("Could not connect to database using user client!");
+        throw err;
+    }
+    else
+    {
+        users = c4.db("Project").collection("users");
+        shops = c4.db("Project").collection("shops");
+        products = c4.db("Project").collection("products");
+    }
+});
+
+
+
 
 router.get('/', (req, res)=>
 {
@@ -76,35 +98,25 @@ router.get('/store/register', (req, res)=>
 router.post('/store/myinfo', (req, res)=>
 {
     //TODO: Handle errors here
-    var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
-
-    c4.connect(err => {
-        // Creates a password hash
-
-        const collection = c4.db("Project").collection("shops");
-
-        // Searches for the user based off of email in the DB.
-        collection.findOne({owner: new ObjectId(req.session.userid)}, function (err, docs) {
-            if(docs !== null)
-            {
-                // Load hash from your password DB.
-                res.send({
-                    userdata: docs,
-                });
-            }
-            else
-            {
-                res.send({
-                    userdata: {
-                        message: "Logged in user does not own a store",
-                        error: err,
-                    }
-                })
-            }
-        });
+    // Searches for the user based off of email in the DB.
+    shops.findOne({owner: new ObjectId(req.session.userid)}, function (err, docs) {
+        if(docs !== null)
+        {
+            // Load hash from your password DB.
+            res.send({
+                userdata: docs,
+            });
+        }
+        else
+        {
+            res.send({
+                userdata: {
+                    message: "Logged in user does not own a store",
+                    error: err,
+                }
+            })
+        }
     });
-
-    c4.close();
 });
 
 // Creates a user in the database when correct information is given
@@ -116,31 +128,23 @@ router.post('/store/update', (req, res)=>
     }
     else
     {
-        var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
+        console.log(req.body);
 
-        c4.connect(err => {
-            const collection = c4.db("Project").collection("shops");
-
-            console.log(req.body);
-
-            //TODO: Make this work
-            // collection.updateOne(
-            //     { owner: req.body.email },
-            //     { $set: {
-            //             first_name: req.body.firstname,
-            //             last_name: req.body.lastname,
-            //             favorite_store: req.body.favorite,
-            //             account_type: req.body.account_type,
-            //             diet: req.body.restrictions,
-            //         } },
-            //     { upsert: true },
-            //     function (resp, err) {
-            //         res.redirect('/settings');
-            //     }
-            // );
-        });
-
-        c4.close();
+        //TODO: Make this work
+        // collection.updateOne(
+        //     { owner: req.body.email },
+        //     { $set: {
+        //             first_name: req.body.firstname,
+        //             last_name: req.body.lastname,
+        //             favorite_store: req.body.favorite,
+        //             account_type: req.body.account_type,
+        //             diet: req.body.restrictions,
+        //         } },
+        //     { upsert: true },
+        //     function (resp, err) {
+        //         res.redirect('/settings');
+        //     }
+        // );
     }
 });
 
@@ -149,47 +153,38 @@ router.post('/store/update', (req, res)=>
 router.post('/store/request', (req, res)=>
 {
     //TODO: Handle errors here
-    var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
+    // Creates a password hash
+    bcrypt.hash(req.body.pass, saltRounds, function(err, hash) {
+        var user = {
+            "email": req.body.email,
+            "first_name": req.body.firstname,
+            "last_name": req.body.lastname,
+            "username": req.body.user,
+            "password": hash
+        };
 
-    c4.connect(err => {
-        // Creates a password hash
-        bcrypt.hash(req.body.pass, saltRounds, function(err, hash) {
-            const users = c4.db("Project").collection("users");
-            const shops = c4.db("Project").collection("shops");
+        // Stores the user in the DB
+        users.insertOne(user, function (err, docs) {
+            console.log("User created...");
 
-            var user = {
-                "email": req.body.email,
-                "first_name": req.body.firstname,
-                "last_name": req.body.lastname,
-                "username": req.body.user,
-                "password": hash
+            var store = {
+                "name": req.body.storename,
+                "category": req.body.category,
+                "location": req.body.location,
+                "owner": docs.ops[0]._id,
+                "description": req.body.description,
+                "address": req.body.address,
             };
 
-            // Stores the user in the DB
-            users.insertOne(user, function (err, docs) {
-                console.log("User created...");
+            shops.insertOne(store, function (err, docs) {
+                console.log("Store created...");
 
-                var store = {
-                    "name": req.body.storename,
-                    "category": req.body.category,
-                    "location": req.body.location,
-                    "owner": docs.ops[0]._id,
-                    "description": req.body.description,
-                    "address": req.body.address,
-                };
-
-                shops.insertOne(store, function (err, docs) {
-                    console.log("Store created...");
-
-                    // Sends user back to the login page
-                    res.redirect('/login');
-                });
-
+                // Sends user back to the login page
+                res.redirect('/login');
             });
+
         });
     });
-
-    c4.close();
 });
 
 router.get('/settings', (req, res)=>
@@ -270,41 +265,33 @@ router.get('/user/password', (req, res)=>
  */
 router.post('/user/myinfo', (req, res)=>
 {
-    var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
-
-    c4.connect(err => {
-
-        const collection = c4.db("Project").collection("users");
-
-        // Stores the user in the DB
-        collection.findOne({email: req.session.user.email}, function (err, docs) {
-            if(!err)
-            {
-                // Load hash from your password DB.
-                res.send({
-                    // TODO: Get all user information to populate settings information
-                    userdata: {
-                        email: docs.email,
-                        username: docs.username,
-                        country: docs.country,
-                        first_name: docs.first_name,
-                        last_name: docs.last_name,
-                        favorite_store: docs.favorite_store,
-                        diet: docs.diet,
-                    }
-                });
-            }
-            else
-            {
-                res.send({
-                    userdata: {
-                        error: err,
-                    }
-                })
-            }
-        });
+    // Stores the user in the DB
+    users.findOne({email: req.session.user.email}, function (err, docs) {
+        if(!err)
+        {
+            // Load hash from your password DB.
+            res.send({
+                // TODO: Get all user information to populate settings information
+                userdata: {
+                    email: docs.email,
+                    username: docs.username,
+                    country: docs.country,
+                    first_name: docs.first_name,
+                    last_name: docs.last_name,
+                    favorite_store: docs.favorite_store,
+                    diet: docs.diet,
+                }
+            });
+        }
+        else
+        {
+            res.send({
+                userdata: {
+                    error: err,
+                }
+            })
+        }
     });
-    c4.close();
 });
 
 /*
@@ -314,40 +301,32 @@ router.post('/user/myinfo', (req, res)=>
  */
 router.post('/user/info', (req, res)=>
 {
-    var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
-
-    c4.connect(err => {
-
-        const collection = c4.db("Project").collection("users");
-
-        // Searches for the user based off of email in the DB.
-        collection.findOne({email: req.body.email}, function (err, docs) {
-            if(docs !== null)
-            {
-                // Load hash from your password DB.
-                res.send({
-                    // TODO: Get all user information to populate settings information
-                    userdata: {
-                        email: docs.email,
-                        username: docs.username,
-                        country: docs.country,
-                        first_name: docs.first_name,
-                        last_name: docs.last_name,
-                    }
-                });
-            }
-            else
-            {
-                res.send({
-                    userdata: {
-                        message: "User with the email '" + req.body.email + "' does not exist",
-                        error: err,
-                    }
-                })
-            }
-        });
+    // Searches for the user based off of email in the DB.
+    users.findOne({email: req.body.email}, function (err, docs) {
+        if(docs !== null)
+        {
+            // Load hash from your password DB.
+            res.send({
+                // TODO: Get all user information to populate settings information
+                userdata: {
+                    email: docs.email,
+                    username: docs.username,
+                    country: docs.country,
+                    first_name: docs.first_name,
+                    last_name: docs.last_name,
+                }
+            });
+        }
+        else
+        {
+            res.send({
+                userdata: {
+                    message: "User with the email '" + req.body.email + "' does not exist",
+                    error: err,
+                }
+            })
+        }
     });
-    c4.close();
 });
 
 // Creates a user in the database when correct information is given
@@ -359,109 +338,85 @@ router.post('/user/updateprofile', (req, res)=>
     }
     else
     {
-        var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
+        console.log(req.body);
 
-        c4.connect(err => {
-            const collection = c4.db("Project").collection("users");
-
-            console.log(req.body);
-
-            collection.updateOne(
-                { email: req.body.email },
-                { $set: {
-                        first_name: req.body.firstname,
-                        last_name: req.body.lastname,
-                        favorite_store: ObjectId(req.body.favorite),
-                        account_type: req.body.account_type,
-                        diet: req.body.restrictions,
-                    } },
-                { upsert: true },
-                function (resp, err) {
-                    res.redirect('/settings');
-                }
-            );
-        });
-
-        c4.close();
-    }
+        users.updateOne(
+            { email: req.body.email },
+            { $set: {
+                    first_name: req.body.firstname,
+                    last_name: req.body.lastname,
+                    favorite_store: ObjectId(req.body.favorite),
+                    account_type: req.body.account_type,
+                    diet: req.body.restrictions,
+                } },
+            { upsert: true },
+            function (resp, err) {
+                res.redirect('/settings');
+            }
+        );
+     }
 });
 
 // Creates a user in the database when correct information is given
 router.post('/user/create', (req, res)=>
 {
-    var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
 
     // TODO: May want to validate some of the data before we store the user here
-    c4.connect(err => {
-        // Creates a password hash
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-            const collection = c4.db("Project").collection("users");
+    // Creates a password hash
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        var user = {
+            email: req.body.email,
+            first_name: req.body.firstname,
+            last_name: req.body.lastname,
+            username: req.body.user,
+            password: hash,
+            account_type: "user",
+            favorite_store: ""
+        };
 
-            var user = {
-                email: req.body.email,
-                first_name: req.body.firstname,
-                last_name: req.body.lastname,
-                username: req.body.user,
-                password: hash,
-                account_type: "user",
-                favorite_store: ""
-            };
-
-            // Stores the user in the DB
-            collection.insertOne(user, function (err, docs) {
-                console.log("User created...");
-                console.log(err);
-            });
+        // Stores the user in the DB
+        users.insertOne(user, function (err, docs) {
+            console.log("User created...");
+            console.log(err);
         });
-
-        // Sends user back to the registration page
-        res.redirect('/user/register');
     });
 
-    c4.close();
+    // Sends user back to the registration page
+    res.redirect('/user/register');
 });
 
 // Authenticates a user at the login page
 router.post('/user/auth', (req, res)=>
 {
-    var c4 = new Mclient("mongodb+srv://admin:thisisanadmin@cluster0-yknsv.mongodb.net", {useNewUrlParser:true});
-
-    c4.connect(err => {
-
-        const collection = c4.db("Project").collection("users");
-
-        // Stores the user in the DB
-        collection.findOne({email: req.body.email}, function (err, docs) {
-            // Checks to make sure an account was found
-            if(docs !== null)
-            {
-                // Load hash from your password DB.
-                bcrypt.compare(req.body.password, docs.password, function(err, bres) {
-                    // res == true or false
-                    if(bres)
-                    {
-                        // TODO: Fix so the user's information isn't stored in plain text
-                        console.log(req.body.email + ' authenticated...');
-                        req.session.user = docs;
-                        req.session.userid = docs._id;
-                        res.redirect('/');
-                    }
-                    else
-                    {
-                        console.log('Password is incorrect...');
-                        res.redirect('/login');
-                    }
-                });
-            }
-            else
-            {
-                console.log("Account does not exist...");
-                res.redirect('/login');
-            }
-        });
+    // Stores the user in the DB
+    users.findOne({email: req.body.email}, function (err, docs) {
+        // Checks to make sure an account was found
+        if(docs !== null)
+        {
+            // Load hash from your password DB.
+            bcrypt.compare(req.body.password, docs.password, function(err, bres) {
+                // res == true or false
+                if(bres)
+                {
+                    // TODO: Fix so the user's information isn't stored in plain text
+                    console.log(req.body.email + ' authenticated...');
+                    req.session.user = docs;
+                    req.session.userid = docs._id;
+                    res.redirect('/');
+                }
+                else
+                {
+                    console.log('Password is incorrect...');
+                    res.redirect('/login');
+                }
+            });
+        }
+        else
+        {
+            console.log("Account does not exist...");
+            res.redirect('/login');
+        }
     });
-
-    c4.close();
 });
 
 // Logs a user out and removes their session data
@@ -470,6 +425,7 @@ router.post('/signout', (req, res)=>
     console.log("Logging out " + req.session.user.email);
     req.session.user = null;
     res.redirect('/login');
+    c4.close(); // Closes the DB connection
 });
 
 module.exports = router;
