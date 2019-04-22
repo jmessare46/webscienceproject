90bf8590fb579b5f322e89e088e38c0f4924abf0
jmessare46 <jmessare46@gmail.com>
2019-04-22 13:43:43 -0400

    Set up removal of item from store products

--- js/inventory.js
+++ js/inventory.js
@@ -1,18 +1,48 @@
 const app = angular.module("myApp", []);
+var data = null;
+
 app.controller('mainController', ['$scope','$http',($scope, $http)=>
 {
-    // Loads all of the store's items into the select
-  $http.get('/store/products').then(
-      function (response) {
-          console.log(response.data.products);
-          $('#item').empty();
-          $('#item').append("<option value=''>Choose...</option>");
 
-          for(var x = 0; x < response.data.products.length; x++)
-          {
-              $('#item').append("<option value='" + response.data.products[x]._id + "'>" +
-                  response.data.products[x].name + "</option>");
-          }
-      }
-  );
+    // Loads all of the store's items into the select
+    $http.get('/store/products').then(
+        function (response) {
+            data = response.data.products;
+
+            $('#item').empty();
+            $('#item').append("<option value=''>Choose...</option>");
+
+            for(var x = 0; x < response.data.products.length; x++)
+            {
+                $('#item').append("<option value='" + response.data.products[x]._id + "'>" +
+                    response.data.products[x].name + "</option>");
+            }
+        }
+    );
 }]);
+
+// Populates the price and description of an item
+function selectitem(item)
+{
+    $('#itemprice').empty();
+    $('#itemdescription').empty();
+
+    if(item === "")
+    {
+        $('#results').css('display', 'none');
+    }
+    else
+    {
+        for(var x = 0; x < data.length; x++)
+        {
+            if(item === data[x]._id)
+            {
+                $('#itemprice').append(data[x].price);
+                $('#itemdescription').append(data[x].description);
+                $('#name').val(data[x].name);
+                $('#id').val(data[x]._id);
+            }
+        }
+        $('#results').css('display', 'block');
+    }
+}
