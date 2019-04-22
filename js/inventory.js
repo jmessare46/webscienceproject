5b2461f13a8b09b8a0342e248fdefb38c27acd80
arronlook <33300146+arronlook@users.noreply.github.com>
2019-04-22 16:55:31 -0400

    inventory should be complete now

--- js/inventory.js
+++ js/inventory.js
@@ -1,11 +1,37 @@
 const app = angular.module("myApp", []);
-var data = null;
+var data = [];
+
+function displayItem()
+{
+  let id = $("#item").val();
+  if ($("#item").val() != "")
+  {
+    for (let i = 0; i < data.length; i++)
+    {
+      if (data[i]._id == id)
+      {
+        $("#itemprice").val(data[i].price);
+        $("#itemdescription").val(data[i].description);
+        $("#results").removeClass("d-none");
+        $("#rm").removeClass("d-none");
+        break;
+      }
+    }
+  }
+  else
+  {
+    $("#results").addClass("d-none");
+    $("#rm").addClass("d-none");
+  }
+}
 
 app.controller('mainController', ['$scope','$http',($scope, $http)=>
 {
 
     // Loads all of the store's items into the select
-    $http.get('/store/products').then(
+    $scope.refresh = function()
+    {
+        $http.get('/store/products').then(
         function (response) {
             data = response.data.products;
 
@@ -17,12 +43,48 @@ app.controller('mainController', ['$scope','$http',($scope, $http)=>
                 $('#item').append("<option value='" + response.data.products[x]._id + "'>" +
                     response.data.products[x].name + "</option>");
             }
-        }
-    );
+        });
+    };
+
+    $scope.refresh();
+
+    $scope.remove = function()
+    {
+      if (($("option:checked")[0].innerHTML != "Choose...") && ($("#item").val() != ""))
+      {
+        $http.post("/product/remove", {"name":$("option:checked")[0].innerHTML, "_id":$("#item").val()})
+        .then(
+        function(response)
+        {
+          alert(response.data.message);
+          $scope.refresh();
+          $("#results").addClass("d-none");
+          $("#rm").addClass("d-none");
+        },
+        function(err)
+        {
+          alert(err.data.message);
+        });
+      }
+    };
+
+    $scope.update = function()
+    {
+      $http.post("/product/update", {"_id":$("#item").val(), "name":$("option:selected")[0].innerHTML, "price":$("#itemprice").val(), "description":$("#itemdescription").val()})
+      .then(
+      function(res)
+      {
+        alert(res.data.message);
+      },
+      function(err)
+      {
+        alert(err.data.message);
+      });
+    }
 }]);
 
 // Populates the price and description of an item
-function selectitem(item)
+/*function selectitem(item)
 {
     $('#itemprice').empty();
     $('#itemdescription').empty();
@@ -45,4 +107,4 @@ function selectitem(item)
         }
         $('#results').css('display', 'block');
     }
-}
+}*/
