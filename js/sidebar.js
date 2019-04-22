24b20f8d0207f83bc9b5090059f54d6e022d3aca
WestonMJones <45551532+WestonMJones@users.noreply.github.com>
2019-04-22 10:00:15 -0400

    Sidebar frontend

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
 
-	// $.ajax({
- //          type: "POST",
- //          url: "/user/isowner",
- //          async: true,
- //          data: {
- //            "email": "WestonMJones@gmail.com"
- //          },
- //          success : function(data) {
- //            alert(data.message);
- //          }
- //    });
+	
+
+
+	$.ajax({
+          type: "GET",
+          url: "/user/isowner",
+          async: true,
+          success : function(data) {
+            var IsOwner = data.is_owner;
+            if (IsOwner==true) {
+            	//alert("Is an owner!");
+            	$(".customer_specific").css("display", "none");
+            	$("#type").html("Vendor");
+            } else {
+            	//alert("Is not an owner!");
+            	$(".vendor_specific").css("display", "none");
+            	$("#type").html("Customer");
+
+            }
+            
+          }
+    });
+
+    $.ajax({
+          type: "POST",
+          url: "/user/myinfo",
+          async: true,
+          success : function(data) {
+            $("#username").html(data.userdata.first_name + " " + data.userdata.last_name);
+
+          }
+    });
 
 
 });
